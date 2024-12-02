c7f5d12615f89a833867a654a7076990b55824c7
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-02 12:21:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+
+	let body=document.getElementById("my-body");
+
+	const text=document.createTextNode("DOM load success");
+
+	body.appendChild(text);
+})
